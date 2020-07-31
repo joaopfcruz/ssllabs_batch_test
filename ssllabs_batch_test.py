5743766a892b1e26ee0891d57289849e88a9eaef
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2020-07-31 21:00:30 +0100

    Minor changes

--- ssllabs_batch_test.py
+++ ssllabs_batch_test.py
@@ -63,31 +63,33 @@ def test_url(url):
     SSLLABS_API_ANALYZE_INITIAL_PARAMS["host"] = url
     logger.info("Starting test on '%s'. Please wait..." % url)
     analyze_data = api_request(SSLLABS_API_ANALYZE_ENDPOINT, SSLLABS_API_ANALYZE_INITIAL_PARAMS)
-    if analyze_data["status"] in [SSLLABS_API_ANALYZE_ENDPOINT_INPROGRESS_STATUS, SSLLABS_API_ANALYZE_ENDPOINT_DNS_STATUS]:
-        retries = 0
-        while (retries < POLLING_MAX_RETRIES and analyze_data["status"] != SSLLABS_API_ANALYZE_ENDPOINT_READY_STATUS):
-            logger.info("Waiting %d seconds until next polling..." % POLLING_TIMEOUT)
-            time.sleep(POLLING_TIMEOUT)
-            SSLLABS_API_ANALYZE_POLLING_PARAMS["host"] = url
-            analyze_data = api_request(SSLLABS_API_ANALYZE_ENDPOINT, SSLLABS_API_ANALYZE_POLLING_PARAMS)
-            if analyze_data["status"] in [SSLLABS_API_ANALYZE_ENDPOINT_INPROGRESS_STATUS, SSLLABS_API_ANALYZE_ENDPOINT_DNS_STATUS]:
-                logger.info("Host status (%s:%d): %s" % (analyze_data["host"], analyze_data["port"], analyze_data["status"]))
-                for e in analyze_data["endpoints"]:
-                        logger.info("\tEndpoint status (%s%s): %s [Progress: %s%%; ETA: %ssecs]" % (e["ipAddress"], "/%s" % e["serverName"] if "serverName" in e else "", e["statusMessage"], str(e["progress"]) if "progress" in e else "--", str(e["eta"]) if "eta" in e else "--"))
-            elif analyze_data["status"] == SSLLABS_API_ANALYZE_ENDPOINT_ERROR_STATUS:
-                logger.error("SSLLabs returned an error: %s" % analyze_data["statusMessage"])
-                break
-            retries += 1
+    if analyze_data:
+        if analyze_data["status"] in [SSLLABS_API_ANALYZE_ENDPOINT_INPROGRESS_STATUS, SSLLABS_API_ANALYZE_ENDPOINT_DNS_STATUS]:
+            retries = 0
+            while (retries < POLLING_MAX_RETRIES and analyze_data["status"] != SSLLABS_API_ANALYZE_ENDPOINT_READY_STATUS):
+                logger.info("Waiting %d seconds until next polling..." % POLLING_TIMEOUT)
+                time.sleep(POLLING_TIMEOUT)
+                SSLLABS_API_ANALYZE_POLLING_PARAMS["host"] = url
+                analyze_data = api_request(SSLLABS_API_ANALYZE_ENDPOINT, SSLLABS_API_ANALYZE_POLLING_PARAMS)
+                if analyze_data:
+                    if analyze_data["status"] in [SSLLABS_API_ANALYZE_ENDPOINT_INPROGRESS_STATUS, SSLLABS_API_ANALYZE_ENDPOINT_DNS_STATUS]:
+                        logger.info("Host status (%s:%d): %s" % (analyze_data["host"], analyze_data["port"], analyze_data["status"]))
+                        for e in analyze_data["endpoints"]:
+                                logger.info("\tEndpoint status (%s%s): %s [Progress: %s%%; ETA: %ssecs]" % (e["ipAddress"], "/%s" % e["serverName"] if "serverName" in e else "", e["statusMessage"], str(e["progress"]) if "progress" in e else "--", str(e["eta"]) if "eta" in e else "--"))
+                    elif analyze_data["status"] == SSLLABS_API_ANALYZE_ENDPOINT_ERROR_STATUS:
+                        logger.error("SSLLabs returned an error: %s" % analyze_data["statusMessage"])
+                        break
+                retries += 1
 
-        if analyze_data["status"] == SSLLABS_API_ANALYZE_ENDPOINT_READY_STATUS:
-            logger.info("Test finished for %s:%d." % (analyze_data["host"], analyze_data["port"]))
-            return analyze_data
-        else:
-            logger.error("Giving up on testing %s:%d." % (analyze_data["host"], analyze_data["port"]))
-            return None
-    elif analyze_data["status"] == SSLLABS_API_ANALYZE_ENDPOINT_ERROR_STATUS:
-            logger.error("SSLLabs returned an error: %s" % analyze_data["statusMessage"])
-            return None
+            if analyze_data["status"] == SSLLABS_API_ANALYZE_ENDPOINT_READY_STATUS:
+                logger.info("Test finished for %s:%d." % (analyze_data["host"], analyze_data["port"]))
+                return analyze_data
+            else:
+                logger.error("Giving up on testing %s:%d." % (analyze_data["host"], analyze_data["port"]))
+                return None
+        elif analyze_data["status"] == SSLLABS_API_ANALYZE_ENDPOINT_ERROR_STATUS:
+                logger.error("SSLLabs returned an error: %s" % analyze_data["statusMessage"])
+                return None
 
 ## MAIN ##
 if __name__ == "__main__":
@@ -108,10 +110,10 @@ if __name__ == "__main__":
             with open(options.in_file) as f_in:
                 for url in f_in:
                     url = url.rstrip()
-                    analyze_data = test_url(url)
-                    with open("%s_%s.json" % (datetime.now().strftime("%Y_%m_%d_%H_%M_%S"), url), "w") as f_out:
-                        f_out.write(json.dumps(analyze_data, indent=4))
-
+                    if len(url) > 0:
+                        analyze_data = test_url(url)
+                        with open("%s_%s.json" % (datetime.now().strftime("%Y_%m_%d_%H_%M_%S"), url), "w") as f_out:
+                            f_out.write(json.dumps(analyze_data, indent=4))
         else:
             logger.fatal("Error checking SSLLabs engine availability. Exiting.")
             os.sys.exit(-1)
